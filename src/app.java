ee620d061c605754a74a70394ac6f1ca638df428
Hui Yao <leehuiyao@outlook.com>
2023-09-18 11:31:12 +0800

    Updated variable names in app.java

--- src/app.java
+++ src/app.java
@@ -2,11 +2,11 @@ import java.sql.SQLOutput;
 
 public class app {
     public static void main(String[] args) {
-        String output;
-        String text = "f ()*+,-./";
-        System.out.println("String to encode "+ text);
-        output =function.encode(text);
-        System.out.println("Encoded String "+ output);
-        System.out.println("Decoded String "+ function.decode(output));
+        String encodedText;
+        String plainText = "f ()*+,-./";
+        System.out.println("String to encode "+ plainText);
+        encodedText =function.encode(plainText);
+        System.out.println("Encoded String "+ encodedText);
+        System.out.println("Decoded String "+ function.decode(encodedText));
     }
 }
